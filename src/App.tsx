154b3dd89d7972cfe08fc68910f5bcfc97e530fc
fadarash <fadarash@gmail.com>
2022-05-18 11:31:08 +0430

    initialize react-redux-typeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,8 @@
-import React, { FC, FormEvent, useState } from "react";
+import React from "react";
 import "./App.css";
-import { InputField } from "./components/InputField";
-import { TodoList } from "./components/TodoList";
-import { Todo } from "./model";
 
-const App: FC = () => {
-  const [todo, setTodo] = useState<string>("");
-  const [todos, setTodos] = useState<Todo[]>([]);
-
-  const handleAdd = (e: FormEvent) => {
-    e.preventDefault();
-    if (todo) {
-      setTodos([...todos, { id: Date.now(), todo: todo, isDone: false }]);
-      setTodo("");
-      console.log(todos);
-    }
-  };
-
-  return (
-    <div className="App">
-      <span className="heading">taskify</span>
-      <InputField
-        todo={todo}
-        setTodo={setTodo}
-        handleAdd={(e) => handleAdd(e)}
-      />
-      <TodoList todos={todos} setTodos={setTodos} />
-    </div>
-  );
+const App = () => {
+  return <div className="App"></div>;
 };
 
 export default App;
